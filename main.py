5f32ac2961c50b65fd97dbca91a6e57b79cf7f3b
Lucasx899 <28308722-Lucasx899@users.noreply.replit.com>
2023-11-03 09:26:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from bs4 import BeautifulSoup
+import requests
+
+url = requests.get('https://realpython.com/atom.xml')
+
+soup = BeautifulSoup(url.content, 'xml')
+entries = soup.find_all('entry')
+
+for entry in entries:
+  title = entry.title.text
+  summary = entry.summary.text
+  link = entry.link['href']
+  print(f'Title: {title}\n\nSummary: {summary}\n\nLink: {link}\n\n-------------')
